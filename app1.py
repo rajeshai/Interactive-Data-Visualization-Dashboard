9de0723512b58ca7dfbfbfd55e35153fceeb4890
rajeshai <73292655+rajeshai@users.noreply.github.com>
2021-08-08 12:59:51 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -45,7 +45,7 @@ def run():
     st.sidebar.subheader("When and Where are users tweeting from?")
     hour = st.sidebar.slider("Hour of day", 0,23)
     modified_data = data[data['tweet_created'].dt.hour == hour]
-    if not st.sidebar.checkbox("Close", True, key=1):
+    if not st.sidebar.checkbox("Close", True, key='1'):
         st.markdown("### Tweets locations based on the time of date")
         st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour+1)%24))
         st.map(modified_data)
